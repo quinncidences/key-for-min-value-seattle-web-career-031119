87ce97d38cd970ebe50a9e7a16a42c8718ca3175
Quinn Cox <qrc.inpa@gmail.com>
2019-03-05 05:20:15 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,21 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.length == 0
+    nil
+  else
+    array = []
+    name_hash.each do |name, num|
+      array << num
+    end
+    low_number = array[0]
+    array.each do |num|
+      low_number = num if num < low_number
+    end
+    name_hash.each do |name, num|
+      if low_number == num
+        return name
+      end
+    end
+  end
 end
